2eb2e520aec74132718433ad8b2b5aec8206ca67
JuanAM8 <juanam9k@gmail.com>
2019-12-06 12:03:37 +0100

    Un bonito easing para las secciones y ahora tenemos colores azules

--- webManager.js
+++ webManager.js
@@ -130,3 +130,19 @@ function detectmob() {
      }
      //Reference: https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
 }
+
+// Everything but smooth Scrolling
+$('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+    //Reference: https://codepen.io/brianhaferkamp/details/xqyeQP
+
